140e31e383e5f59c779c384fa8e948b1fd0dc83c
pradeev-ssp <pradeev812@gmail.com>
2025-07-20 15:28:30 +0530

    update

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b"Hello, DevOps! after github" in response.data
+    assert b"Pradeev" in response.data
