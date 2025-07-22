a9482ae7e5788849790e90e923ae3fac72b5b820
pradeev-ssp <pradeev812@gmail.com>
2025-07-22 16:43:41 +0530

    Deploy to ec2

--- flask_1.py
+++ flask_1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps! after github actions"
+    return "Hello, DevOps! after github actions Test 1"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
